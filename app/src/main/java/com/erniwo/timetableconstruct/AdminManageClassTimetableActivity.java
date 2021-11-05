1dbff5011a2d0aecb6f02357b586e838b302c1c4
Erni Wo <2569441w@student.gla.ac.uk>
2021-11-05 15:21:44 +0000

    fix: teacher view

---
+++ app/src/main/java/com/erniwo/timetableconstruct/AdminManageClassTimetableActivity.java
@@ -0,0 +1,14 @@
+package com.erniwo.timetableconstruct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminManageClassTimetableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_manage_class_timetable);
+    }
+}
--- app/src/main/java/com/erniwo/timetableconstruct/TeacherTimetableActivity.java
+++ app/src/main/java/com/erniwo/timetableconstruct/TeacherTimetableActivity.java
@@ -1,18 +1,55 @@
 package com.erniwo.timetableconstruct;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class TeacherTimetableActivity extends AppCompatActivity {
 
-    private Button button;
+    private TextView nameOfUser;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDb;
+    private String userKey;
+    private String currentUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_timetable);
-        button =(Button) findViewById(R.id.TButton);
+
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        mDatabase = FirebaseDatabase.getInstance();
+//        mDb = mDatabase.getReference();
+//        FirebaseUser user = firebaseAuth.getCurrentUser();
+//        userKey = user.getUid();
+//
+//        mDb.child("Users").child(userKey).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                currentUsername = snapshot.child("type").getValue().toString();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+        nameOfUser = (TextView) findViewById(R.id.NameOfUser);
+//        nameOfUser.setText( currentUsername+ "'s Timetable");
+        nameOfUser.setText( "Teacher's Timetable");
+
     }
 }
